95b68e952a954303ec953e8a5cd68b3c34a31272
John-Mike <jmmarquardt@gmail.com>
2016-08-10 16:15:46 -0400

    creating game object

--- assets/scripts/star-wars.js
+++ assets/scripts/star-wars.js
@@ -1,13 +1,15 @@
 var starwars = {
-	playerHealth: 0,
-	playerCounterAttack: 0,
-	playerLives: 1,
-	enemyHealth: 0,
-	enemyCounterAttack: 0;
+
+	playerHealth = 0;
+	playerCounterAttack = 0;
+	playerLives = 1;
+	enemyHealth = 0;
+	enemyCounterAttack = 0;
+	galleryCards = [];
 
 
 	// player constructor class:
-	function Player (name, healthPoints, attackPower, counterAttackPower, 
+	Player: function (name, healthPoints, attackPower, counterAttackPower, 
 		image, blaster, winQuote, losingQuote, loseQuote) {
 		// player attributes
 		this.name = name;
@@ -20,7 +22,18 @@ var starwars = {
 		this.losingQuote = losingQuote;
 		this.loseQuote = loseQuote;
 
+		// this.name: name,
+		// this.healthPoints: healthPoints,
+		// this.attackPower: attackPower,
+		// this.counterAttackPower: counterAttackPower,
+		// this.image: image,
+		// this.blaster: blaster,
+		// this.winQuote: winQuote,
+		// this.losingQuote: losingQuote,
+		// this.loseQuote: loseQuote,
+
 		// player methods
+
 
 	}
 
@@ -28,7 +41,7 @@ var starwars = {
 
 	// players object
 	
-	var galleryCards = [];
+
 
 	galleryCards.push (Player('Luke Skywalker', 100, 6, 6, '../images/luke-skywalker.png',
 	 false, '../sound/luke/whatjunk.wav', '../sound/luke/badfeel1.wav', '../sound/luke/converters.wav'));
@@ -38,7 +51,10 @@ var starwars = {
 
 	galleryCards.push (Player('R2D2', 100, 6, 6, '../images/R2-D2.png',
 	 false, '../sound/R2D2/R2D2a.wav', '../sound/R2D2/R2D2b.wav', '../sound/R2D2/R2D2b.wav'));
-
+	console.log(galleryCards);
+	console.log('gallery card #1= ' + galleryCards[0]);
+	console.log('gallery card #2= ' + galleryCards[1]);
+	console.log('gallery card #3= ' + galleryCards[2]);
 	// galleryCards.push (Player('luke', 100, 6, 6, '../images/luke-skywalker.png',
 	//  false, '../sound/luke/whatjunk.wav', '../sound/luke/badfeel1.wav', '../converters.wav'));
 
@@ -54,10 +70,14 @@ var starwars = {
 
 	// reset code:
 	var reset = function () {
-		playerHealth: 0,
-		playerLives: 1,
-		enemyHealth: 0;
+		var playerHealth = 0;
+		var	playerCounterAttack = 0;
+		var	playerLives = 1;
+		var	enemyHealth = 0;
+		var	enemyCounterAttack = 0;
+		var	galleryCards = [];
+
 		// maybe put in a timeout step
 
-	}
+	};
 }
